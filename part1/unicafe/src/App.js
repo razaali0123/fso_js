ab8da78800277a816f852ee5870b43d6b1c0e82e
Raza Ali <aliiqbal0123@gmail.com>
2021-12-04 23:48:11 +0100

    ex1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
 
 const Header = prop => <h1>{prop.text}</h1>
+const All = prop =>  (prop.good + prop.bad + prop.neutral)
 
+
+const Avg = prop => {
+if (prop.good + prop.bad + prop.neutral === 0){return 0}
+return (prop.good*1 + prop.bad*-1)/(prop.good + prop.bad + prop.neutral)
+}
+
+
+const Positivepercentage = prop => {
+if (prop.good + prop.bad + prop.neutral === 0){return 0}
+return ((prop.good/(prop.good + prop.bad + prop.neutral))*100)
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   
 
   const handlebuttonGood = () => {
@@ -33,6 +44,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all <All good = {good} neutral = {neutral} bad = {bad} /></p>
+      <p>average <Avg good = {good} bad = {bad} neutral = {neutral} /> </p>
+      <p>positive <Positivepercentage good = {good} neutral = {neutral} bad = {bad} /> %</p>
     </div>
   )
 }

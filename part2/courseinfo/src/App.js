b8e081c982f5a7b29b79d8f868c099c1a1f22728
Raza Ali <aliiqbal0123@gmail.com>
2021-12-08 21:35:25 +0100

    ex2.1 done

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Course = (prop)=>{
+  return(
+    <>
+  <Header headline = {prop.course.name} />
+  <Content lst = {prop.course.parts} />
+  </>
+  )
+}
+
+const Header = (prop) =>{
+return <h1>{prop.headline}</h1>
+}
+
+const Content = (prop) =>{
+return (prop.lst.map(x => <Part key = {x.id} partname = {x}/>))
+}
+
+const Part = (prop) =>{
+return <p>{prop.partname.name} {prop.partname.exercises}</p>
+}
+
+
+function App() {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App;

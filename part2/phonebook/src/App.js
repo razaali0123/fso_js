86f38c6d5692e988e032933ebc32f9d5613667e6
Raza Ali <aliiqbal0123@gmail.com>
2021-12-27 22:33:55 +0100

    ex2.12 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,17 +3,82 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
+const Show = (result) => {
+  if (result.lst.length === 0){
+    
+    
+    return <p>No items found</p>
+  }
+  else if (result.lst[0].length === 1){
+    console.log(result.lst[0][0])
+    return(
+  <>
 
+  <h1>{result.lst[0][0].name.common}</h1>
+  <p>Capital {result.lst[0][0].capital}</p>
+  <p>Population {result.lst[0][0].population}</p>
+  <h2>Languages</h2>
+  <ul>
+  {Object.values(result.lst[0][0].languages).map(obj => <li key={obj}>{obj}</li>)}
+  </ul>
+  <img src={result.lst[0][0].flags.png} />
+  </>
+)
+  }
+  else if (result.lst[0].length >=10){
+    return <p>Too many matches, please be specific</p>
+  }
+
+  else if (result.lst[0].length <10){
+    return (
+      <ul>
+    {result.lst[0].map(object =><li key={object.name.common}>{object.name.common}</li>)}
+    </ul>
+    )
+
+  }
+  else{
+    return <p>See Search Results here ...</p>
+  }
+
+}
 
 
 
 const App = () => {
-  const [person, setPerson] = useState([])
+  const [country, setCountry] = useState([])
+  const [search, setSearch] = useState('')
+  const [result, setResult] = useState([]) 
+
+  const Changer = (event) =>{
+    if (event.target.value === ''){
+      setSearch(event.target.value)
+      setResult([])
+      return
+    }
+    else{
+    const regex = new RegExp('^'.concat(event.target.value.toLowerCase()));
+    const filtered = country.filter(obj => regex.test((obj.name.common).toLowerCase()))
+    setSearch(event.target.value)
+    setResult([filtered])
+    }
+  }
+
   const hook = ()=> {
-    axios.get("http://localhost:3001/persons").then(resposes => setPerson(resposes.data))
+    axios.get("https://restcountries.com/v3.1/all").then(resposes =>{
+      setCountry(resposes.data)
+    })
   }
   useEffect(hook, [])
-  return null
+  return (
+    
+  <form>
+    find countries
+  <input type="text" value = {search} onChange = {Changer}/>
+  {<Show lst = {result} />}
+  </form>
+  )
+  
 
 }
 

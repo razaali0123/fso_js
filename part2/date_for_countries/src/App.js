afd97b70b8d6a0b1feea767e59ae44a36418dd8d
Raza Ali <aliiqbal0123@gmail.com>
2022-01-01 09:49:24 +0100

    ex2.12 done

---
+++ part2/date_for_countries/src/App.js
@@ -0,0 +1,101 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+
+
+
+
+
+
+const App = () => {
+  const [country, setCountry] = useState([])
+  const [search, setSearch] = useState('')
+  const [result, setResult] = useState([]) 
+
+  const showMore = (event) =>{
+    console.log([event])
+    setResult([[event]])
+
+  }
+  
+  const Show = (result) => {
+    console.log("checker")
+    console.log(result[0])
+    if (result.lst.length === 0){
+      
+      
+      return <p>No items found</p>
+    }
+    else if (result.lst[0].length === 1){
+      console.log(result.lst[0][0])
+      return(
+    <>
+  
+    <h1>{result.lst[0][0].name.common}</h1>
+    <p>Capital {result.lst[0][0].capital}</p>
+    <p>Population {result.lst[0][0].population}</p>
+    <h2>Languages</h2>
+    <ul>
+    {Object.values(result.lst[0][0].languages).map(obj => <li key={obj}>{obj}</li>)}
+    </ul>
+    <img src={result.lst[0][0].flags.png} />
+    </>
+  )
+    }
+    else if (result.lst[0].length >=10){
+      return <p>Too many matches, please be specific</p>
+    }
+  
+    else if (result.lst[0].length <10){
+      return (
+        <ul>
+      {result.lst[0].map(objectss =><li key={objectss.name.common}>{objectss.name.common}
+      <button onClick={() => showMore(objectss)}>Show More</button>
+      </li>)}
+      </ul>
+      )
+  
+    }
+    else{
+      return <p>See Search Results here ...</p>
+    }
+  
+  }
+
+
+
+  const Changer = (event) =>{
+    if (event.target.value === ''){
+      setSearch(event.target.value)
+      setResult([])
+      return
+    }
+    else{
+    const regex = new RegExp('^'.concat(event.target.value.toLowerCase()));
+    const filtered = country.filter(obj => regex.test((obj.name.common).toLowerCase()))
+    setSearch(event.target.value)
+    setResult([filtered])
+    }
+  }
+
+  const hook = ()=> {
+    axios.get("https://restcountries.com/v3.1/all").then(resposes =>{
+      setCountry(resposes.data)
+    })
+  }
+  useEffect(hook, [])
+  return (
+    
+  <form>
+    find countries
+  <input type="text" value = {search} onChange = {Changer}/>
+  {<Show lst = {result} />}
+  </form>
+  )
+  
+
+}
+
+export default App;
+
